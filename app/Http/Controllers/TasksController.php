221729b5c8910e0f05108a7d77a54e650fa2c0d6
lucianayeli@outlook.com <nayeli.andrade@mtlc.com.mx>
2024-07-04 10:52:48 -0600

    validaciones al crear tareas

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -1,46 +1,47 @@
 <?php
-
 namespace App\Http\Controllers;
 
-use App\Models\Company;
 use App\Models\Task;
-use App\Models\User;
-use App\Rules\TaskRule;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 
 class TasksController extends Controller
 {
     public function store(Request $request)
-{
-    $validatedData = $request->validate([
-        'company_id' => 'required|integer', 
-        'name' => 'required|string',
-        'description' => 'required|string',
-        'user_id' => 'required|integer',
-    ]);
+    {
+        $validator = Validator::make($request->all(), [
+            'company_id' => 'required|integer',
+            'name' => 'required|max:100',
+            'description' => 'required|max:200',
+            'user_id' => 'required|integer',
+        ]);
 
-    $user_id = $validatedData['user_id'];
-    $pendingTasksCount = Task::where('user_id', $user_id)->where('is_completed', false)->count();
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
 
-    if ($pendingTasksCount >= 5) {
-        return response()->json(['error' => 'No se pueden crear más de 5 tareas pendientes por usuario.'], 400);
+        $validatedData = $validator->validated();
+
+        $user_id = $validatedData['user_id'];
+        $pendingTasksCount = Task::where('user_id', $user_id)->where('is_completed', false)->count();
+
+        if ($pendingTasksCount >= 5) {
+            return response()->json(['error' => 'No se pueden crear más de 5 tareas pendientes por usuario.'], 400);
+        }
+
+        $task = Task::create($validatedData);
+
+        $response = [
+            'id' => $task->id,
+            'name' => $task->name,
+            'description' => $task->description,
+            'user' => $task->user->user,
+            'company' => [
+                'id' => $task->company->id,
+                'name' => $task->company->name,
+            ],
+        ];
+
+        return response()->json($response);
     }
-    
-    $task = Task::create($validatedData);
-
-    $response = [
-        'id' => $task->id,
-        'name' => $task->name,
-        'description' => $task->description,
-        'user' => $task->user->user, 
-        'company' => [
-            'id' => $task->company->id,
-            'name' => $task->company->name,
-        ],
-    ];
-    
-    return response()->json($response);
-}
-        
 }
